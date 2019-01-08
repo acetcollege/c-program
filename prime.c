236a2820952f54dc02606ab40bc5e7c6f5963fea
Priyanka kachhap <priyanka19teen@gmail.com>
2019-01-08 11:42:09 +0530

    i have change the line

--- prime.c
+++ prime.c
@@ -3,7 +3,7 @@ int main()
 {
     int n, i, flag = 0;
 
-    printf("Enter a positive integer: ");
+    printf("Enter the positive integer: ");
     scanf("%d", &n);
 
     for(i = 2; i <= n/2; ++i)
